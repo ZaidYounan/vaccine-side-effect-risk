ca966458d6be5f6fad181bec59568db6e3dd3311
ZaidYounan <zaid.younan@gmail.com>
2021-08-02 15:55:49 +1000

    Create checkUnlucky function, style components

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,28 +1,44 @@
 import React from 'react'
+import './Button.css';
+import '../App.css';
 import { useEffect, useState } from 'react';
+
+var min = 1;
+var max = 50000;
+
+const unLucky = Math.floor(Math.random() * (max - min + 1)) + min;
+
 
 function Button() {
     const [ randomNumber, setRandomNumber ] = useState(null);
+    const [ disableButton, setDisableButton ] = useState(false);
 
-    var min = 1;
-    var max = 50000;
+
 
     const handleClick = () => {
-        setRandomNumber(Math.floor(Math.random() * (max - min + 1)) + min)
+        setRandomNumber(Math.floor(Math.random() * (max - min + 1)) + min);
+        setDisableButton(true);
+        setTimeout(() => setDisableButton(false), 500);
     };
-
     
+    const checkUnlucky = () => {
+        if (randomNumber === unLucky) {
+            return <div className="result">You have received bloot clotting!</div>
+        } else if (randomNumber != unLucky && randomNumber != null) {
+            return <div className="result"><strong>You have been safely vaccinated!</strong></div>
+        }
+    }
 
-    
-    var unLucky = 666;
 
     return (
-        <div>
-            <p>If you get {unLucky}, you have received the blood-clotting side effect!</p>
-            <button onClick={handleClick}>
-                Click Here!
+        <div className="container">
+            <h1>The highest estimate for the chance of receiving the blood-clotting side-effect from the COVID-19 Vaccine is 1 in 50,000. For anyone who wants to "roll the dice" to get a feel of how rare this is, I've made this simple tool.</h1>
+            <button className="btn btn-secondary btn-lg" onClick={handleClick} disabled={disableButton}>
+                Take the Vaccine!
             </button>
+            {checkUnlucky()}
             <p>{randomNumber}</p>
+            <p>You have been assigned the unlucky number <strong>{unLucky}</strong>. If you roll this number, that is symbolic of receiving the blood-clotting side-effect from the COVID-19 Vaccine.</p>
         </div>
     )
 }
